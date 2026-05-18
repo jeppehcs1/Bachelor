c3015a52dd8ff880b1237f9e0c034ac738cd5fdb
jeppehcs1 <s235223@dtu.dk>
2026-05-18 09:53:03 +0200

    bugfix

--- Bachelor/Models/Problems/PermutationProblem.cs
+++ Bachelor/Models/Problems/PermutationProblem.cs
@@ -4,8 +4,8 @@ namespace Bachelor.Models.Problems;
 
 using System.Collections.Generic;
 
-public abstract class PermutationProblem(int Dimension) : IProblemType<TSPInstance>
+public abstract class PermutationProblem(int dimension) : IProblemType<TSPInstance>
 {
-    public int Dimension { get; set; }
+    public int Dimension { get; set; } = dimension;
     public abstract int Fitness(TSPInstance c);
 }
